1d334f1c4c2cb4d7b33523e775ec7a55dedf4dac
steelll <steelll@users.noreply.github.com>
2020-05-22 17:35:13 +0900

    add search repository

---
+++ src/main/java/me/steell/miniproject/repository/CustomerSearch.java
@@ -0,0 +1,11 @@
+package me.steell.miniproject.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CustomerSearch {
+    
+    private String name;
+    private String customertype;
+}
