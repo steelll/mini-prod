e27806bdc571fd57561e48b4b299e40db9b531f9
steelll <steelll@users.noreply.github.com>
2020-05-25 13:25:21 +0900

    delete Member

--- src/main/java/me/steell/miniproject/Hello.java
+++
@@ -1,9 +0,0 @@
-package me.steell.miniproject;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class Hello {
-    private String data;
-}
--- src/main/java/me/steell/miniproject/service/CustomerService.java
+++ src/main/java/me/steell/miniproject/service/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
     //회원가입
     @Transactional
     public Long join(final Customer customer) {
-        validateDuplicateCustomer(customer);
+        // validateDuplicateCustomer(customer);
         customerRepository.save(customer);
         return customer.getId();
     }
@@ -37,20 +37,20 @@ public class CustomerService {
         return customerRepository.findByName(name);
     }
 
-    private void validateDuplicateCustomer(final Customer customer) {
-        
-        final List<Customer> findPersons = customerRepository.findByRegnumber(customer.getRegnumber());
-        if (!findPersons.isEmpty()) {
-            throw new IllegalStateException("이미 등록되어 있습니다.");
-            
-        }
-
-        final List<Customer> findCompanys = customerRepository.findByBizregnumber(customer.getBizregnumber());
-        if (!findCompanys.isEmpty()) {
-            throw new IllegalStateException("이미 등록되어 있습니다.");
-            
-        }
-
-    }
+//    private void validateDuplicateCustomer(final Customer customer) {
+//
+//        final List<Customer> findPersons = customerRepository.findByRegnumber(customer.getRegnumber());
+//        if (!findPersons.isEmpty()) {
+//            throw new IllegalStateException("이미 등록되어 있습니다.");
+//
+//        }
+//
+//        final List<Customer> findCompanys = customerRepository.findByBizregnumber(customer.getBizregnumber());
+//        if (!findCompanys.isEmpty()) {
+//            throw new IllegalStateException("이미 등록되어 있습니다.");
+//
+//        }
+//
+//    }
 
 }
